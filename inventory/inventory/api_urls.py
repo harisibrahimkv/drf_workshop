3cfbfcc4b24149981994e3ae11c509a1e571369e
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-18 22:01:08 +0530

    Defines the /users/ endpoint

--- inventory/inventory/api_urls.py
+++ inventory/inventory/api_urls.py
@@ -3,3 +3,11 @@ from rest_framework import routers
 from django.conf.urls import patterns, url
 
 router = routers.SimpleRouter()
+
+router.register(
+    r'users',
+    UserViewSet,
+    base_name='user'
+)
+
+urlpatterns = router.urls
