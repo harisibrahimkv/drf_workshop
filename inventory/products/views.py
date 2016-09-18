d601b81fef363c6812f81a445c16440d939caedf
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-18 22:21:55 +0530

    Implements the UserViewSet

--- inventory/products/views.py
+++ inventory/products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
