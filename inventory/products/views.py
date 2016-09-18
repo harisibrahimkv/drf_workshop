d17fc7c239c672af02ae636ea1257772a9d59f7b
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-18 23:50:46 +0530

    Implements the CategoryViewSet

--- inventory/products/views.py
+++ inventory/products/views.py
@@ -3,10 +3,16 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .models import Cateogry
+
+from .serializers import UserSerializer, CategorySerializer
 
 User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
