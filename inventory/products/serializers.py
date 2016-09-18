20e93bed0aa526e7b0bd756890c2e5b9dba14830
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-18 23:50:57 +0530

    Implements the CategorySerializer

--- inventory/products/serializers.py
+++ inventory/products/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
 
+from .models import Category
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ('password',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
