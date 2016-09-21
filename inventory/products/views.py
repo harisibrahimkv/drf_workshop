7c390853a6494a876f16888e6e3fa673c8f21145
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-21 22:52:42 +0530

    Implements the StatViewSet

--- inventory/products/views.py
+++ inventory/products/views.py
@@ -3,16 +3,27 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets
 
-from .models import Category
+from .models import Category, Stat
 
-from .serializers import UserSerializer, CategorySerializer
+from .serializers import (
+    UserSerializer,
+    CategorySerializer,
+    StatSerializer
+)
 
 User = get_user_model()
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class StatViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Stat.objects.all()
+    serializer_class = StatSerializer
