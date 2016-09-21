ff90be874f3b1ebf766c2501bed9ec8ddfa623c3
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-22 02:09:03 +0530

    Overrides the create method to implement Stat increment

--- inventory/products/views.py
+++ inventory/products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Category, Stat
 
@@ -22,6 +24,19 @@ class UserViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        if Stat.objects.exists():
+            s = Stat.objects.get()
+            s.category_count += 1
+            s.save()
+        else:
+            Stat.objects.create(category_count=1)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class StatViewSet(viewsets.ReadOnlyModelViewSet):
