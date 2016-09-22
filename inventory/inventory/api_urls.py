51eca4fced668e741f21e7adda844f76ef779e39
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-22 23:52:36 +0530

    Nests the product urls under categories urls

--- inventory/inventory/api_urls.py
+++ inventory/inventory/api_urls.py
@@ -29,11 +29,11 @@ router.register(
     base_name='stat'
 )
 
-router.register(
-    r'products',
-    ProductViewSet,
-    base_name='product'
-)
+# router.register(
+#     r'products',
+#     ProductViewSet,
+#     base_name='product'
+# )
 
 ListCreateMapper = {
     'get':'list',
@@ -49,6 +49,8 @@ RetrieveUpdateDestroyMapper = {
 urlpatterns = [
     url(r'^categories/$', CategoryViewSet.as_view(ListCreateMapper), name='category-list'),
     url(r'^categories/(?P<pk>[0-9]+)/$', CategoryViewSet.as_view(RetrieveUpdateDestroyMapper), name='category-detail'),
+    url(r'^categories/(?P<categories_pk>[0-9]+)/products/$', ProductViewSet.as_view(ListCreateMapper), name='product-list'),
+    url(r'^categories/(?P<categories_pk>[0-9]+)/products/(?P<pk>[0-9]+)/$', ProductViewSet.as_view(RetrieveUpdateDestroyMapper), name='caproduct-detail'),
 ]
 
 urlpatterns += router.urls
