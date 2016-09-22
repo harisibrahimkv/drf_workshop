f19d1e5fc0dd457b3373ae77823efbc140875a04
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-22 15:28:46 +0530

    Implements the Product model and  migrations

--- inventory/products/models.py
+++ inventory/products/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     status = models.IntegerField(default=1)
 
+
 class Stat(models.Model):
     category_count = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category)
